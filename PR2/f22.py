a356a254597eac99e0651bbabd8a950219cddf20
Ivan Nikolaev-Axenov <nikolaevaxenov@gmail.com>
2021-02-22 17:44:37 +0300

    Изменения в рабочей папке ПР2

--- PR2/f22.py
+++ PR2/f22.py
@@ -1,2 +1,6 @@
 def f22(x):
     pass
+
+
+print(f"f(0x708d850f) = {f22(0x708d850f)}")
+print(f"f(0xbafd838e) = {f22(0xbafd838e)}")
--- PR2/f23.py
+++ PR2/f23.py
@@ -1,9 +1,9 @@
 def f23(lst):
-    nlst = [[1 if r[0].split('!')[0] == 'Да' else 0,
-          f"{r[2].split()[2]} {r[2].split()[0][:1]}.{r[2].split()[1]}",
-          r[4].replace('/', '-'),
-          f"{r[0].split('!')[1][:2]} {r[0].split('!')[1][2:5]} {r[0].split('!')[1][5:8]}-{r[0].split('!')[1][8:10]}-{r[0].split('!')[1][10:]}"]
-         for r in lst]
+    nlst = [['1' if r[0].split('!')[0] == 'Да' else '0',
+             f"{r[2].split()[2]} {r[2].split()[0][:1]}.{r[2].split()[1]}",
+             r[4].replace('/', '-'),
+             f"{r[0].split('!')[1][:2]} {r[0].split('!')[1][2:5]} {r[0].split('!')[1][5:8]}-{r[0].split('!')[1][8:10]}-{r[0].split('!')[1][10:]}"]
+            for r in lst]
     result = []
     for x in nlst:
         if x not in result:
