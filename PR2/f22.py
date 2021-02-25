9c9181599dcbc867b0c4e41527277b67504ff453
Ivan Nikolaev-Axenov <nikolaevaxenov@gmail.com>
2021-02-25 13:00:24 +0300

    Добавлена задача 2.2 в практической работе №2

--- PR2/f22.py
+++ PR2/f22.py
@@ -1,6 +1,21 @@
 def f22(x):
-    pass
+    F = x & 0xC0000000
+    E = x & 0x20000000
+    D = x & 0x10000000
+    C = x & 0xE000000
+    B = x & 0x1FFFE00
+    A = x & 0x1FF
+
+    B <<= 7
+    E >>= 14
+    A <<= 6
+    C >>= 22
+    D >>= 26
+    F >>= 30
+
+    x = A + B + C + D + E + F
+    return x
 
 
-print(f"f(0x708d850f) = {f22(0x708d850f)}")
-print(f"f(0xbafd838e) = {f22(0xbafd838e)}")
+print(f"f(0x708d850f) = {f22(1888322831)}")
+print(f"f(0xbafd838e) = {f22(3137176462)}")
